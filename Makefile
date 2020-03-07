66d24b36ecc0ec3236066ae945df61cda0a25fe5
Sean Murray <smurra42@jhu.edu>
2020-03-07 16:33:30 -0500

    added image reading capability

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 CC=gcc
 CFLAGS=-std=c99 -pedantic -Wall -Wextra -g
 
+project: project.o ppm_io.o
+	$(CC) project.o ppm_io.o -o project
 
 demo: demo_ppm.o ppm_io.o
 	$(CC) demo_ppm.o ppm_io.o -o demo
+
+project.o: project.c ppm_io.h
+	$(CC) $(CFLAGS) -c project.c
 
 demo_ppm.o: demo_ppm.c ppm_io.h
 	$(CC) $(CFLAGS) -c demo_ppm.c
@@ -12,4 +17,4 @@ ppm_io.o: ppm_io.c ppm_io.h
 	$(CC) $(CFLAGS) -c ppm_io.c
 
 clean:
-	rm -f *.o demo
+	rm -f *.o demo project
--- ppm_io.c
+++ ppm_io.c
@@ -1,11 +1,16 @@
 
-// __Add your name and JHED above__
+// Sean Murray
+// smurra42
+// Daniel Weber
+//
+//
 // ppm_io.c
 // 601.220, Spring 2019
-// Starter code for midterm project - feel free to edit/add to this file
+//
 
 #include <assert.h>
 #include "ppm_io.h"
+#include <stdlib.h>
 
 
 
@@ -17,8 +22,61 @@ Image * read_ppm(FILE *fp) {
 
   // check that fp is not NULL
   assert(fp); 
+  // Tag
+  char tag1;
+  char tag2;
+  // Taking in Tag
+  fscanf(fp, "%c%c", &tag1, &tag2);
+  // Checking for proper PPM tag
+  if (!(tag1 == 'P' && tag2 == '6')) {
+	fprintf(stderr, "1Invalid PPM file.\n");
+	return NULL;
+  }
+ 
+  int row;
+  int col;
+  int colors;
+  char input;
 
-  return NULL;  //TO DO: replace this stub
+  // Taking in newline char and #col
+  // If this fails, then there is a comment
+  if (fscanf(fp, "%c%d", &input, &col) != 2) {
+	// Iterating through comment
+	do {
+		fscanf(fp, "%c", &input);
+	} while (input != '\n'); 
+	// Taking in correct integer for #col
+	fscanf(fp, "%d", &col);
+  }
+
+  // Obtaining #row and #colors
+  fscanf(fp, "%d%d", &row, &colors);
+
+  // Checking for proper format
+  if (row <= 0 || col <= 0 || colors != 255) {
+	fprintf(stderr, "2Invalid PPM file.\n");
+	return NULL;
+  }
+  // Taking in last whitespace char before binary data
+  fscanf(fp, "%c", &input);
+  
+  // At this point, actual image data starts
+  // Allocating space for image with correct dimensions
+  Image * im = (Image *) malloc(sizeof(Image));
+  im->rows = row;
+  im->cols = col;
+  im->data = (Pixel *) malloc(sizeof(Pixel) * row * col);
+  
+  // Reading image data
+  int numRead = fread(im->data, sizeof(Pixel), row * col, fp);
+  // Checking for proper reading 
+  if (numRead != row * col) {
+	fprintf(stderr, "3Failed to read image data.\n");
+	return NULL;
+  }
+
+  // Returning populated Image
+  return im;
   
 }
 
